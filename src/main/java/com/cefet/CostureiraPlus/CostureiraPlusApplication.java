02d91e6fff1b83454e1efa5ade590daa46d23541
Pedro Augusto Ramos da Silva <pedrosilva220113@gmail.com>
2025-06-12 13:41:43 -0300

    first commit

---
+++ src/main/java/com/cefet/CostureiraPlus/CostureiraPlusApplication.java
@@ -0,0 +1,13 @@
+package com.cefet.CostureiraPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostureiraPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostureiraPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cefet/CostureiraPlus/CostureiraPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.CostureiraPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostureiraPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
